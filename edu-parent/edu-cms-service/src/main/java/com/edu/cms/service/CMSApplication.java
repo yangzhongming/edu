c47bcbd20ccfb237386e8bb9ec952e039d47fbf0
yangzhongming <yzhming@sina.cn>
2019-02-19 18:23:55 +0800

    init

---
+++ edu-parent/edu-cms-service/src/main/java/com/edu/cms/service/CMSApplication.java
@@ -0,0 +1,11 @@
+package com.edu.cms.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMSApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CMSApplication.class, args);
+	}
+}
